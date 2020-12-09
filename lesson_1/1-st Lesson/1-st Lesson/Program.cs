2675244ec3875d29f26ba91c7b3521aa23b0f9d8
Andrew <tehMono2@gmail.com>
2020-12-10 02:22:49 +0500

    Lesson1(Practice)

---
+++ lesson_1/1-st Lesson/1-st Lesson/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_st_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.WriteLine("Привет, как тебя зовут?");       //Вывод на консоль 
+                string name = Console.ReadLine();                   //Считываем и записываем в переменную (name)
+                Console.WriteLine($"Привет! {name}, сегодня [{DateTime.Now}] ");
+                Console.ReadLine();
+            
+        }
+    }
+
+}
+   
