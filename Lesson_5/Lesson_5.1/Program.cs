241f1d987b6d4b35cdacabd88987eacb00d83bd4
Andrew <tehMono2@gmail.com>
2020-12-28 09:33:03 +0500

    Lesson-5

---
+++ Lesson_5/Lesson_5.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите произвольный набор данных: ");
+            string text = Console.ReadLine();
+            string myFile = "text.txt";
+            File.WriteAllText(myFile, text);
+
+            Console.Write("В вашем файле сейчас записано: ");
+            string readFile = File.ReadAllText(myFile);
+            Console.Write(readFile);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
